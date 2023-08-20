3977a0688d148140edd89a6651829cd9254f4650
unknown <n.anovukela@gmail.com>
2023-08-21 00:24:46 +0200

    clean up of files and creating gui

--- tracking/urls.py
+++ tracking/urls.py
@@ -2,9 +2,20 @@ from django.urls import path
 from tracking import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("hello/<name>", views.hello_there, name="hello_there"),
-    path('load/records',views.load_csv_data,name='load'),
-    path('api/records/', views.get_records, name='get_records'),
+    path("", views.home, name="home"),#replace with load
+    path("hello/<name>", views.hello_there, name="hello_there"),#remove
+    path('load/records',views.load_csv_data,name='load'),#use the home path ie the first url for the load
+    path('api/records/', views.get_records, name='get_records'),#might need to be removed
     path('api/create/', views.create_record, name='create_record'),
+    path('api/json',views.GetDataAsJson,name="json"),
+    path('table',views.getTable,name="table"),
+    path('delete/<int:id>',views.delete,name="delete"),
+    path("update/<int:id>",views.updateRecord,name="update"),
 ]
+ #   path('pullData/', views.fetchData, name='pullData'),
+ #   path('saveData/',views.SaveData,name ='saveData'),
+ #   path('queryDb/',views.GetData,name='queryDB'),
+  #  path('table/delete/<int:id>',views.delete,name ='delete'),
+  #  path('table/',views.getTable,name='table'),
+   # path('table/update/<int:id>',views.update,name='update'),
+   # path('table/update/updaterecord/<int:id>', views.updaterecord, name='updateRecord'),
--- tracking/views.py
+++ tracking/views.py
@@ -2,14 +2,17 @@ import re
 from django.utils.timezone import datetime
 from django.http import HttpResponse
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from django.shortcuts import get_object_or_404, render
 from django.http import JsonResponse
 from .models import TrackingData
+from django.contrib import messages
 import pandas as pd
 
 from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect
-#from api_package.serializers import ApiSerializer
+from api_serializer.serializer import apiSerializer
 from django.template import loader
 
 def home(request):
@@ -51,7 +54,16 @@ def hello_there(request, name):
             'date': datetime.now()
         }
     )
-def get_records(request):
+
+#this function pulls the data from db as json and exposes it in browser
+@api_view(['GET'])
+def GetDataAsJson(request):
+    all_records= TrackingData.objects.all()
+    jsonTracking =apiSerializer(all_records,many=True)
+    return Response(jsonTracking.data)
+
+
+def get_records(request):# you can put this one in a context and html file
     records = TrackingData.objects.all()
     data =[
     {"vehicleid" :TrackingData.vehicleid,
@@ -66,8 +78,29 @@ def get_records(request):
     "gps_fix_type":TrackingData.gps_fix_type
     } for record in records]
 
-    return JsonResponse(data, safe=False)
+    return JsonResponse(data, safe=False)#instead you would say return(request,context,html)"""
 
+def getTable(request):
+   records = TrackingData.objects.all().values()
+   context = {
+    'records': records,
+   }
+   return render(request,'tracking/table.html', context)#edit table
+
+@api_view(['DELETE'])
+def delete(request, id):
+    try:
+        #records = TrackingData.objects.get(id=id)
+        records = get_object_or_404(TrackingData, id=id)
+        print(records)
+        records.delete()
+
+        return Response({'message': f'Data with id {id} has been deleted.'})
+    except TrackingData.DoesNotExist:
+        return Response({'error': f'Data with id {id} does not exist.'}, status=404)
+
+
+#this view is for creating a single new record for tracking data
 def create_record(request):
     if request.method == 'POST':
         vehicleid= request.POST.get('vehicleid')
@@ -86,4 +119,19 @@ def create_record(request):
                                             speed=speed,heading=heading,ignitionState=ignitionState,
                                             gps_accuracy=gps_accuracy,gps_fix_type=gps_fix_type)
         return JsonResponse({"message": "trackingData created successfully."})
-    return JsonResponse({"message": "Invalid request."})
+    #return JsonResponse({"message": "Invalid request."})#render form wwith inputs
+    return render(request, 'tracking/newRecord.html') 
+
+def updateRecord(request, id):
+    try:
+        coordinate_longitude = request.POST.get('coordinate_longitude')
+        coordinate_latitude = request.POST.get('coordinate_latitude')
+        record = TrackingData.objects.get(id=id)
+        record.coordinate_longitude = coordinate_longitude
+        record.coordinate_latitude= coordinate_latitude
+        record.save()
+        return HttpResponseRedirect(reverse('table')) 
+    except Exception as e:
+         error_message = f"The id does not exist: {str(e)}"
+
+    return render(request, 'tracking/errorMessage.html', {'error_message': error_message}) 
